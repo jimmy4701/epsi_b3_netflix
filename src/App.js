56e064bb91f571704272d59c233d627f3136a044
Jérémy SIMONKLEIN ANGELY <jeremy.simonklein@gmail.com>
2021-01-08 12:59:37 +0100

    Added MovieHeader component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, {useState} from 'react'
 import './App.css';
+import MovieHeader from './components/MovieHeader'
 
 const App = () => {
+  const [color, setColor] = useState("red")
+
+  const main_movie = {
+    title: "Avengers",
+    type: "Film"
+  }
+
+  const toggleColor = () => {
+    if(color === 'red'){
+      setColor("blue")
+    }else{
+      setColor('red')
+    }
+  }
 
   return (
     <div className="App">
-      
+      <MovieHeader movie={main_movie} color={color} />
+      <button onClick={toggleColor}>Changer la couleur</button>
     </div>
   );
 }
---
+++ src/components/MovieHeader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MovieHeader = ({movie, color}) => {
+
+    return(
+        <p>MOVIE HEADER {movie.title} {color}</p>
+    )
+}
+
+export default MovieHeader
